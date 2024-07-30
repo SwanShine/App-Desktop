27774ad8954fb814c2de65d3cc2bcd56d8220237
Wellington Ayres <wlayres07@gmail.com>
2024-07-29 22:59:32 -0300

    correção na barra lateral

--- Side Bar.py
+++ Side Bar.py
@@ -1,87 +1,83 @@
+import customtkinter as Ctk
 from tkinter import PhotoImage
-import tkinter as tk
 
-# dictionary of colors:
-color = {"nero": "#252726", "orange": "#FF8700", "darkorange": "#FE6101"}
+# Dicionário de cores
+color = {"white": "#FFFFFF", "orange": "#FF8700"}
 
-# setting root window:
-root = tk.Tk()
+# Configuração da janela principal
+root = Ctk.CTk()
 root.title("Tkinter Navbar")
-root.config(bg="gray17")
 root.geometry("400x600+850+50")
 
-# setting switch state:
+# Estado do botão de alternância
 btnState = False
 
-# loading Navbar icon image:
+# Carregamento das imagens dos ícones
 navIcon = PhotoImage(file="open.png")
 closeIcon = PhotoImage(file="close.png")
 
-# setting switch function:
+
+# Função para alternar o estado do Navbar
 def switch():
     global btnState
-    if btnState is True:
-        # create animated Navbar closing:
+    if btnState:
+        # Fechar Navbar de forma animada
         for x in range(301):
             navRoot.place(x=-x, y=0)
-            topFrame.update()
-
-        # resetting widget colors:
-        brandLabel.config(bg="gray17", fg="green")
-        homeLabel.config(bg=color["orange"])
-        topFrame.config(bg=color["orange"])
-        root.config(bg="gray17")
-
-        # turning button OFF:
+            root.update_idletasks()
+        
+        # Resetar as cores dos widgets
+        homeLabel.configure(fg_color=color["orange"])
+        topFrame.configure(fg_color=color["orange"])
+        root.configure(fg_color=color["white"])
+        
+        # Desligar o botão
         btnState = False
     else:
-        # make root dim:
-        brandLabel.config(bg=color["nero"], fg="#5F5A33")
-        homeLabel.config(bg=color["nero"])
-        topFrame.config(bg=color["nero"])
-        root.config(bg=color["nero"])
-
-        # created animated Navbar opening:
-        for x in range(-300, 0):
+        # Diminuir a janela principal
+        homeLabel.configure(fg_color=color["white"])
+        topFrame.configure(fg_color=color["white"])
+        root.configure(fg_color=color["white"])
+        
+        # Abrir Navbar de forma animada
+        for x in range(-300, 1):
             navRoot.place(x=x, y=0)
-            topFrame.update()
-
-        # turing button ON:
+            root.update_idletasks()
+        
+        # Ligar o botão
         btnState = True
 
-# top Navigation bar:
-topFrame = tk.Frame(root, bg=color["orange"])
-topFrame.pack(side="top", fill=tk.X)
+# Barra de navegação superior
+topFrame = Ctk.CTkFrame(root, fg_color=color["orange"], height=60)
+topFrame.pack(side="top", fill="x")
 
-# Header label text:
-homeLabel = tk.Label(topFrame, text="PE", font="Bahnschrift 15", bg=color["orange"], fg="gray17", height=2, padx=20)
-homeLabel.pack(side="right")
+# Rótulo de cabeçalho
+homeLabel = Ctk.CTkLabel(topFrame, text="SwanShine", font=("Bahnschrift", 15), fg_color=color["orange"], text_color=color["white"], height=60, padx=20)
+homeLabel.pack(side="right", padx=10)
 
-# Main label text:
-brandLabel = tk.Label(root, text="Pythonista\nEmpire", font="System 30", bg="gray17", fg="green")
-brandLabel.place(x=100, y=250)
-
-# Navbar button:
-navbarBtn = tk.Button(topFrame, image=navIcon, bg=color["orange"], activebackground=color["orange"], bd=0, padx=20, command=switch)
+# Botão de Navbar
+navbarBtn = Ctk.CTkButton(topFrame, image=navIcon, fg_color=color["orange"], hover_color=color["orange"], command=switch, width=40, height=40, text="")
 navbarBtn.place(x=10, y=10)
 
-# setting Navbar frame:
-navRoot = tk.Frame(root, bg="gray17", height=1000, width=300)
+# Configuração do frame Navbar
+navRoot = Ctk.CTkFrame(root, fg_color=color["white"], height=600, width=300)
 navRoot.place(x=-300, y=0)
-tk.Label(navRoot, font="Bahnschrift 15", bg=color["orange"], fg="black", height=2, width=300, padx=20).place(x=0, y=0)
+Ctk.CTkLabel(navRoot, text="", font=("Bahnschrift", 15), fg_color=color["orange"], text_color=color["white"], height=60, width=300).place(x=0, y=0)
 
-# set y-coordinate of Navbar widgets:
+# Coordenada y dos widgets da Navbar
 y = 80
-# option in the navbar:
-options = ["Profile", "Settings", "Help", "About", "Feedback"]
-# Navbar Option Buttons:
-for i in range(5):
-    tk.Button(navRoot, text=options[i], font="BahnschriftLight 15", bg="gray17", fg=color["orange"], activebackground="gray17", activeforeground="green", bd=0).place(x=25, y=y)
-    y += 40
 
-# Navbar Close Button:
-closeBtn = tk.Button(navRoot, image=closeIcon, bg=color["orange"], activebackground=color["orange"], bd=0, command=switch)
+# Opções no Navbar
+options = ["Profile", "Configurações", "Contato", "Sobre"]
+
+# Botões de opções no Navbar
+for option in options:
+    Ctk.CTkButton(navRoot, text=option, font=("Bahnschrift Light", 15), fg_color=color["orange"], hover_color=color["white"], text_color=color["white"], width=250, height=40).place(x=25, y=y)
+    y += 50
+
+# Botão de fechar Navbar
+closeBtn = Ctk.CTkButton(navRoot, text="", image=closeIcon, fg_color=color["white"], hover_color=color["white"], command=switch, width=40, height=40)
 closeBtn.place(x=250, y=10)
 
-# window in mainloop:
+# Iniciar o loop principal da janela
 root.mainloop()
