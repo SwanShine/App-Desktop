e29facc72b9b15cb8d1ed3179baa2e36630bb467
Wellington Ayres <145718802+Wlayres@users.noreply.github.com>
2024-08-26 12:02:48 -0300

    corrindo erro tela de login

--- SwanShine.py
+++ SwanShine.py
@@ -458,7 +458,7 @@ def open_administracao():
     entry_nome = entries_adicionar['Nome']
     entry_cpf = entries_adicionar['CPF']
     entry_email = entries_adicionar['Email']
-    entry_endereco = entries_adicionar['endereco']
+    entry_endereco = entries_adicionar['endereço']
     entry_telefone = entries_adicionar['Telefone']
     entry_usuario = entries_adicionar['Usuário']
     entry_senha = entries_adicionar['Senha']
@@ -608,19 +608,19 @@ bg_lab.grid(row=0, column=0)
 frame1 = CTkFrame(tela_login,fg_color="white", bg_color="white", height=350, width=300,corner_radius=20)
 frame1.grid(row=0, column=1,padx=40)
 
-title = CTkLabel(frame1,text="Login",text_color="Black",font=("",35,"bold"))
+title = CTkLabel(frame1,text="Login",text_color="Black",font=("",35))
 title.grid(row=0,column=0,sticky="nw",pady=30,padx=100)
 
 input_usuario = CTkEntry(frame1,text_color="black", placeholder_text="Username", fg_color="white", placeholder_text_color="black",
-                         font=("",16,"bold"), width=200, corner_radius=15, height=45)
+                         font=("",16), width=200, corner_radius=15, height=45)
 input_usuario.grid(row=1,column=0,sticky="nwe",padx=30)
 
 input_senha = CTkEntry(frame1,text_color="black",placeholder_text="Password",fg_color="white",placeholder_text_color="black",
-                         font=("",16,"bold"), width=200,corner_radius=15, height=45, show="*")
+                         font=("",16), width=200,corner_radius=15, height=45, show="*")
 input_senha.grid(row=2,column=0,sticky="nwe",padx=30,pady=20)
 
 
-l_btn = CTkButton(frame1,text="Login",font=("",15,"bold"),height=40,width=60,fg_color="#FF66C4",cursor="hand2",
+l_btn = CTkButton(frame1,text="Login",font=("",15),height=40,width=60,fg_color="#FF66C4",cursor="hand2",
                   corner_radius=15,command=login_valido_tela_selecionar_usuario,)
 l_btn.grid(row=3,column=0,sticky="ne",pady=20, padx=100)
 
